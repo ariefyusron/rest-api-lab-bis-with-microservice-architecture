014984d8b14c82a65deb155f2f45ecc373e6f2ab
Arief Yusron <ariefyusron98@gmail.com>
2019-02-07 05:46:59 +0700

    add relation service auth to profile, add cors to profile

--- auth/app/controllers/auth.js
+++ auth/app/controllers/auth.js
@@ -1,8 +1,18 @@
+const axios = require('axios')
+
 const authModel = require('../models/auth')
+const urlToProfile = process.env.URL_PROFILE
 
 const register = async (req,res) => {
   try{
     const newUser = await authModel.create(req.body)
+    const userByNim = newUser
+    const token = req.jwt.sign({userByNim},req.secret_key)
+    await axios.post(urlToProfile, {}, {
+      headers:{
+        Authorization: `Bearer ${token}`
+      }
+    })
     res.json(newUser)
   } catch{
     res.status(400).json({
--- profile/index.js
+++ profile/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const validator = require('express-validator')
 const mongoose = require('mongoose')
@@ -9,11 +10,16 @@ const secret_key = process.env.JWT_SECRET
 
 const port = 5000
 const route = require('./app/routes')
+const corOptions = {
+  origin: 'http://localhost:4000',
+  optionsSuccessStatus: 200
+}
 
 //connect to mongoDb with mongoose
 mongoose.connect('mongodb://lab_bis_service_profile:ariefyusron98@ds125125.mlab.com:25125/lab_bis_profiles')
 mongoose.Promise = global.Promise
 
+app.use(cors(corOptions))
 app.use((req,res,next) => {
   req.jwt = jwt
   req.secret_key = secret_key
