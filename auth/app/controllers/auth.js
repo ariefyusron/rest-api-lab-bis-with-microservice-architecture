f101f03dabe6b1b063ae332c8a0caddc53fcbf83
Arief Yusron <ariefyusron98@gmail.com>
2019-02-07 02:09:01 +0700

    update auth service, add login

--- auth/app/controllers/auth.js
+++ auth/app/controllers/auth.js
@@ -12,7 +12,27 @@ const register = async (req,res) => {
 }
 
 const login = async (req,res) => {
-  res.json('login')
+  const userByNim = await authModel.findOne({
+    nim: req.body.nim
+  })
+  if(userByNim){
+    const compare = req.bcrypt.compareSync(req.body.password,userByNim.password)
+    if(compare){
+      const token = req.jwt.sign({userByNim},req.secret_key)
+      res.json({
+        userData: userByNim,
+        token: token
+      })
+    } else{
+      res.status(400).json({
+        message: 'Password invalid'
+      })
+    }
+  } else{
+    res.status(400).json({
+      message: 'NIM invalid'
+    })
+  }
 }
 
 module.exports = {
